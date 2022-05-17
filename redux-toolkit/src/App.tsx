812b454e395e60e99b7d6763b3408635aa08cc7d
David Groomes <dwgroomes@gmail.com>
2022-05-16 19:47:03 -0500

    Style columns on hover
    Use CSS grid.

--- redux-toolkit/src/App.tsx
+++ redux-toolkit/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     const count = useAppSelector(state => state.counter.value)
     const dispatch = useAppDispatch()
 
-    // Create the rows of visual elements that make up the Connect Four grid structure.
+    // Create the columns of visual elements that make up the Connect Four grid structure.
     //
     // Note: Because we're using React, the creation of React elements that contain child elements has to be done in a
     // nested expression. Why? See the discussion in this StackOverflow question titled "How to append child to React element?"
@@ -16,10 +16,11 @@ function App() {
     // And how do we write an expression that creates an array of some length? It's surprisingly obscure. See the
     // discussion in this StackOverflow question titled "functional way to iterate over range (ES6/7)"
     // https://stackoverflow.com/questions/30650961/functional-way-to-iterate-over-range-es6-7
-    const connectFourRows = [1, 2, 3, 4, 5, 6].map(() => {
-        return (<div>
-            {[1, 2, 3, 4, 5, 6, 7].map(() => {
-                return <img src={circle} className="connect-four-circle" alt="connect-four-space"/>
+    const connectFourColumns = [1, 2, 3, 4, 5, 6, 7].map(columnIndex => {
+        return (<div className="connect-four-column">
+            {[1, 2, 3, 4, 5, 6].map(rowIndex => {
+                return <img src={circle} className="connect-four-circle"
+                            style={{gridColumn: columnIndex, gridRow: rowIndex}} alt="connect-four-space"/>
             })
             }</div>)
     })
@@ -36,7 +37,9 @@ function App() {
                     Let's play Connect Four!
                 </p>
             </header>
-            {connectFourRows}
+            <div className="connect-four-grid">
+                {connectFourColumns}
+            </div>
         </div>
     )
 }
